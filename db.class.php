9cd0418c933df8b936f0d39ffe809af0079a5a1c
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2013-07-25 17:12:34 -0500

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -1,6 +1,6 @@
 <?php
 class DataBase {
-  private $stmt;
+	private $stmt;
 
 	private $host	= DB_HOST;
 	private $user	= DB_USER;
@@ -16,10 +16,10 @@ class DataBase {
 
 		// Set options
 		$options = array(
-			PDO::ATTR_PERSISTENT			=> true,
-			PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_PERSISTENT		=> true,
+			PDO::ATTR_ERRMODE		=> PDO::ERRMODE_EXCEPTION,
 			PDO::MYSQL_ATTR_INIT_COMMAND	=> 'SET NAMES utf8',
-			PDO::ATTR_EMULATE_PREPARES		=> false
+			PDO::ATTR_EMULATE_PREPARES	=> false
 		);
 
 		try {
