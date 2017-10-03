113f7c737733c9945cf4fdfc3cf05da54b9c853d
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-10-03 03:27:47 -0500

    Setup table data whitin test

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -13,74 +13,132 @@ namespace AlfredoRamos\Tests;
 
 use AlfredoRamos\PDODb\PDODb;
 use PHPUnit\Framework\TestCase;
+use PDOException;
 
+/**
+ * @group basic
+ */
 class BasicTest extends TestCase {
 
-	protected $db;
+	/** @var \AlfredoRamos\PDODb\PDODb $pdodb */
+	protected static $pdodb;
 
-	protected function setUp() {
-		parent::setUp();
+	/** @var string $table */
+	protected static $table;
 
-		$this->db = new PDODb([
+	/**
+	 * @backupGlobals enabled
+	 */
+	public static function setUpBeforeClass() {
+		// Database instance
+		self::$pdodb = new PDODb([
 			'user'		=> $GLOBALS['DB_USER'],
 			'database'	=> $GLOBALS['DB_DBNAME'],
 			'prefix'	=> $GLOBALS['DB_TPREFIX']
 		]);
+
+		// Table name
+		self::$table = $GLOBALS['DB_TABLE'];
+
+		// Drop test table
+		$sql = 'DROP TABLE IF EXISTS ' . self::$pdodb->prefix . self::$table;
+		self::$pdodb->query($sql);
+		self::$pdodb->execute();
+
+		// Create test table
+		$sql = 'CREATE TABLE ' . self::$pdodb->prefix . self::$table . ' (
+			id int(11) NOT NULL AUTO_INCREMENT,
+			first_name varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+			last_name varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+			city varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+			PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci';
+		self::$pdodb->query($sql);
+		self::$pdodb->execute();
+
+		// Insert initial data
+		$users = [
+			['Maria', 'Anders', 'Germany'],
+			['Ana', 'Trujillo', 'Mexico'],
+			['Thomas', 'Hardy', 'United Kingdom'],
+			['Patricia', 'McKenna', 'Ireland'],
+			['Frédérique', 'Citeaux', 'France']
+		];
+		$sql = 'INSERT INTO ' . self::$pdodb->prefix . self::$table . '
+				(first_name, last_name, city)
+				VALUES (:first_name, :last_name, :city)';
+		self::$pdodb->query($sql);
+
+		self::$pdodb->beginTransaction();
+		foreach ($users as $user) {
+			self::$pdodb->bindArray([
+				':first_name'	=> $user[0],
+				':last_name'	=> $user[1],
+				':city'			=> $user[2]
+			]);
+			self::$pdodb->execute();
+		}
+		self::$pdodb->endTransaction();
 	}
 
 	public function testInstance() {
-		$this->assertInstanceOf(PDODb::class, $this->db);
+		$this->assertInstanceOf(PDODb::class, self::$pdodb);
+	}
+
+	public function testInvalidInstance() {
+		$this->expectException(PDOException::class);
+		$db = new PDODb;
 	}
 
 	public function testTableExists() {
-		$sql = 'SHOW TABLES LIKE "' . $this->db->prefix . $GLOBALS['DB_TABLE'] . '"';
-		$this->db->query($sql);
-		$this->assertFalse(empty($this->db->fetch()));
+		$sql = 'SHOW TABLES LIKE "' . self::$pdodb->prefix . self::$table . '"';
+		self::$pdodb->query($sql);
+		$this->assertFalse(empty(self::$pdodb->fetch()));
 	}
 
 	public function testTotalUsers() {
 		$sql = 'SELECT COUNT(id) as total_users
-			FROM ' . $this->db->prefix . $GLOBALS['DB_TABLE'];
-		$this->db->query($sql);
-		$this->assertEquals(5, $this->db->fetchField('total_users'));
+			FROM ' . self::$pdodb->prefix . self::$table;
+		self::$pdodb->query($sql);
+		$this->assertEquals(5, self::$pdodb->fetchField('total_users'));
 	}
 
 	public function testInsertToTable() {
-		$sql = 'INSERT INTO ' . $this->db->prefix . $GLOBALS['DB_TABLE'] . '
+		$sql = 'INSERT INTO ' . self::$pdodb->prefix . self::$table . '
 			(first_name, last_name, city)
 			VALUES (:first_name, :last_name, :city)';
-		$this->db->query($sql);
-		$this->db->bindArray([
+		self::$pdodb->query($sql);
+		self::$pdodb->bindArray([
 			':first_name'	=> 'Yang',
 			':last_name'	=> 'Wang',
 			':city'			=> 'Switzerland'
 		]);
-		$this->db->execute();
-		$this->assertEquals(6, $this->db->lastInsertId());
+		self::$pdodb->execute();
+		$this->assertEquals(6, self::$pdodb->lastInsertId());
 	}
 
 	public function testFetchColumn() {
 		$sql = 'SELECT id, city
-			FROM ' . $this->db->prefix . $GLOBALS['DB_TABLE'] . '
+			FROM ' . self::$pdodb->prefix . self::$table . '
 			WHERE last_name = :last_name';
-		$this->db->query($sql);
-		$this->db->bind(':last_name', 'Trujillo');
-		$this->assertEquals(2, $this->db->fetchColumn());
-		$this->assertEquals('Mexico', $this->db->fetchColumn(1));
+		self::$pdodb->query($sql);
+		self::$pdodb->bind(':last_name', 'Trujillo');
+		$this->assertEquals(2, self::$pdodb->fetchColumn());
+		$this->assertEquals('Mexico', self::$pdodb->fetchColumn(1));
 	}
 
 	public function testRowCount() {
-		$sql = 'SELECT id FROM ' . $this->db->prefix . $GLOBALS['DB_TABLE'];
-		$this->db->query($sql);
-		$this->db->execute();
-		$this->assertEquals(6, $this->db->rowCount());
+		$sql = 'SELECT id FROM ' . self::$pdodb->prefix . self::$table;
+		self::$pdodb->query($sql);
+		self::$pdodb->execute();
+		$this->assertEquals(6, self::$pdodb->rowCount());
 	}
 
 	public function testColumnCount() {
-		$sql = 'SELECT * FROM ' . $this->db->prefix . $GLOBALS['DB_TABLE'];
-		$this->db->query($sql);
-		$this->db->execute();
-		$this->assertEquals(4, $this->db->columnCount());
+		$sql = 'SELECT * FROM ' . self::$pdodb->prefix . self::$table;
+		self::$pdodb->query($sql);
+		self::$pdodb->execute();
+		$this->assertEquals(4, self::$pdodb->columnCount());
 	}
 
 }
