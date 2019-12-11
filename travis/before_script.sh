065d0322dd72d72c8342f5cd63a7e5cc9fbe7273
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-12-11 12:01:31 -0600

    Fix MariaDB issues
    https://travis-ci.community/t/mariadb-build-error-with-xenial/3160

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -6,6 +6,7 @@ set -e
 case "${DB}" in
 	mariadb|mysql)
 		mysql -u root < travis/data/test_db_mysql.sql
+		mysql -u root < travis/data/fix_permissions.sql
 		;;
 	postgresql)
 		psql < travis/data/test_db_postgresql.sql
