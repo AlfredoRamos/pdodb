faffa71a8c8eccf268423cd0e4e4ffab1d0183ef
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-12-13 21:09:32 -0600

    Cleanup

--- .github/workflows/sql/test_table_mysql.sql
+++ .github/workflows/sql/test_table_mysql.sql
@@ -1,11 +1,5 @@
-DROP DATABASE IF EXISTS test_db;
-
-CREATE DATABASE IF NOT EXISTS test_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 USE test_db;
-
 DROP TABLE IF EXISTS t_users;
-
 CREATE TABLE t_users (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	first_name varchar(50) COLLATE utf8_unicode_ci NOT NULL,
